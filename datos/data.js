dfe9e0300458e5d97d46cfe6c4c7974d95740757
Alain-Ochoa <75461397+Alain-Ochoa@users.noreply.github.com>
2022-07-09 22:43:19 -0500

    cambio

---
+++ datos/data.js
@@ -0,0 +1,87 @@
+
+export const data =[
+    {
+        question: 'LA ZONA O PLACA BASAL ESTA CONSTITUIDA POR RESTOS DE SINCITIOTROFOBLASTO Y CITOTROFOBLASTO QUE AL DEGENERAR SE LLENA DE FRIBRINOIDE, EL MISMO QUE CONDENSADO CONSTITUYE:',
+        choices: [
+            'EL ESPACIO INTERVELLOSO',
+            'LAS VELLOSIDADES CORIALES',
+            'EL EPITELIO AMNIOTICO',
+            'LA CAPA DE NITABUCH',
+            'LA GELATINA DE WHARTON'
+        ],
+        answer:'LA CAPA DE NITABUCH'
+    },
+
+    {
+        question: 'EL DIÁMETRO DEL CRÁNEO FETAL QUE MIDE 9.5 CM, CORRESPONDE A:',
+        choices: [
+            'OCCIPITO FRONTAL',
+            'SUBOCCIPITO BREGMATICO',
+            'BITEMPORAL',
+            'SUBCCIPITO MENTONIANO',
+            'SUPRAOCCIPITO MENTONIANO'
+        ],
+        answer:'SUBOCCIPITO BREGMATICO'
+    },
+    {
+        question: 'DURANTE EL EMBARAZO, A PARTIR DE LAS DOCE SEMANAS, EL APORTE CALORICO DEBE INCREMENTARSE EN:',
+        choices: [
+            '600 CALORIAS',
+            '400 CALORIA',
+            '500 CALORIAS',
+            '200 CALORIAS',
+            '300 CALORIAS'
+        ],
+        answer:'200 CALORIAS'
+    },
+    {
+        question: 'EL APORTE CALÓRICO DEBE INCREMENTARSE EN 1,000 CALORÍAS DIARIAS',
+        choices: [
+            'LA PUBERTAD',
+            'LA ETAPA ADULTA',
+            'LA GESTACIÓN',
+            'LA LACTACIÓN',
+            'LA NIÑEZ'
+        ],
+        answer:'LA LACTACIÓN'
+    },
+    {
+        question: 'EL APORTE SUFICIENTE DE PROTEÍNAS TIENE UNA GRAN IMPORTANCIA PARA PREVENIR:',
+        choices: [
+            'MALFORMACIONES FETALES',
+            'MALFORMACIONES MATERNAS',
+            'MALFORMACIONES DEL ÚTERO',
+            'TUBERCULOSIS',
+            'EDEMA Y LA HIPOPROTEINEMIA'
+        ],
+        answer:'EDEMA Y LA HIPOPROTEINEMIA'
+    },
+    {
+        question: 'DURANTE EL EMBARAZO EL AUMENTO DE PESO MATERNO DURANTE EL SEGUNDO TRIMESTRE DEBE PROMEDIAR MENSUALMENTE, LOS:',
+        choices: [
+            '500 GRAMOS',
+            '800 GRAMOS',
+            '1200 GRAMOS',
+            '1500 GRAMOS',
+            '2000 GRAMOS'
+        ],
+        answer:'1200 GRAMOS'
+    },
+    {
+        question: 'DURANTE EL EMBARAZO, LA ALIMENTACIÓN DEFICITARIA DE LA MADRE, CUANDO EL VALOR CALÓRICO DE LA ALIMENTACIÓN ES INFERIOR A LAS NECESIDADES DE CONSUMO, PREDISPONE A LAS DEFORMIDADES DEL NIÑO, ABORTO, ADEMÁS DE:',
+        choices: [
+            'PARTO PREMATURO Y ÓBITO FETAL',
+            'NIÑOS CON SINDROME DE DOWN',
+            'FETOS CON ANOMALIAS CROMOSOMICAS',
+            'FETOS MACROSÓMICOS',
+            'RECIÉN NACIDOS GRANDES PARA LA EDAD GESTACIONAL'
+        ],
+        answer:'PARTO PREMATURO Y ÓBITO FETAL'
+    },
+]
+   
+
+
+
+
+
